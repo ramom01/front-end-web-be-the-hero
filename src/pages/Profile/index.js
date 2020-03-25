dca9701933b2d1f69533fe1f81516098e72e07a5
Ramon Rodrigues <rrsalescc@hotmail.com>
2020-03-25 18:36:51 -0300

    Comunicaçao da rota NewIncident com o back-end e finalizacao do projeto

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import './styles.css'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import logoImg from '../../assets/logo.svg'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 import api from '../../services/api'
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([])
+
     const ongId = localStorage.getItem('ongId')
     const ongName = localStorage.getItem('ongName')
+
+    const history = useHistory()
 
     useEffect(() => {
         api.get('profile', {
@@ -34,6 +37,12 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear()
+
+        history.push('/')
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -41,7 +50,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
